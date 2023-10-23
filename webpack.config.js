86738db22c5a6870610d6dfdd71d152d54f03f5f
Weibing Chen <weibing.chen.xy@gmail.com>
2023-10-22 19:35:05 -0700

    introduce the teaser and comprehsion

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
     entry: "./script.js",
@@ -6,4 +8,9 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    plugins: [new CompressionPlugin()],
 };
